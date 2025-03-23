7339373095cb5ba1d6c905ee682056f2ae6bd96c
SHDPIXEL <shdpixel@gmail.com>
2025-03-23 20:30:37 +0530

    Update Api.js

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from "axios";
 
-export const API_BASE_URL = "https://2ej9ufdjyb.ap-south-1.awsapprunner.com/";tus
+export const API_BASE_URL = "https://2ej9ufdjyb.ap-south-1.awsapprunner.com/";
 // export const API_BASE_URL = "http://192.168.1.11:4040";
 
 export const BASE_IMAGE_URL = "https://2ej9ufdjyb.ap-south-1.awsapprunner.com"
